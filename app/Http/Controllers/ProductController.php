e58437ae3bbfdb482e713c65b7719c0925511652
alwan <mardika200@gmail.com>
2024-07-22 21:24:19 +0700

    Insert Data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,9 +8,17 @@ use App\Models\Product;
 // import return type view
 use Illuminate\View\View;
 
+// import return type redirect Response
+use Illuminate\Http\RedirectResponse;
+
+// import Http Request
+// digunakan untuk menerima request
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
-    public function index() : View{
+    public function index(): View
+    {
 
         //get all products
         // untuk mendapatkan semua produk
@@ -23,5 +31,49 @@ class ProductController extends Controller
         // products.index nanti akan berada di folder view
         // methode compact untuk mengirim data ke folder view
         return view('products.index', compact('products'));
+    }
+
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        // untuk validasi
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        // gambar akan disimpan di folder storage -> public ->products,
+        // -hashname artinya nama gambar akan di random
+        $image->storeAs('public/products', $image->hashName());
+
+        //create product
+        // proses memasukan data
+        Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        //redirect to index
+        // jika berhasil akan redirect ke route product.index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Products - SantriKoding.com</title>
+    <title>CRUD Simple</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -15,8 +15,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
-                    <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
+                    <h3 class="text-center my-4">Belajar Laravel 11</h3>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
